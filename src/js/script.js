090e4254c2d12f3d5dd5dbc60767236d69687505
Guilherme Henrique <guiihenriquecs@gmail.com>
2021-10-18 17:33:55 -0300

    v2

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
 const calculate = document.getElementById('calculate');
 
 $(document).ready(function () {
-    $('.weightAthlete').mask("#00", {
-      reverse: true
-    });
-  })
-  
-  $(document).ready(function () {
-    $('#timeTravelled').mask('00:00:00');
+  $('.weightAthlete').mask("#00", {
+    reverse: true
+  });
+})
+
+$(document).ready(function () {
+  $('#timeTravelled').mask('00:00:00');
 })
 
 
@@ -16,28 +16,28 @@ const physical = () => {
   const weight = document.getElementById('weightAthlete').value
   const distance = document.getElementById('distanceTravelled').value
   const time = document.getElementById('timeTravelled').value
-  
+
   const result = document.getElementById('result')
   const caloryValue = 0.0175;
 
-  var arrayTime = time.split(":").map(function(item) {
+  var arrayTime = time.split(":").map(function (item) {
     return parseInt(item, 10);
   });
 
   const hour = arrayTime[0]
-  const minute = arrayTime[1]/60
-  const second = arrayTime[2]/3600
-  
+  const minute = arrayTime[1] / 60
+  const second = arrayTime[2] / 3600
+
   const totalHour = hour + minute + second;
   const hourToMinutes = totalHour * 60;
 
-  const pace = (hourToMinutes/distance).toFixed(2)
-  const velocityMedium = (distance/totalHour).toFixed(2);
+  const pace = (hourToMinutes / distance).toFixed(2)
+  const velocityMedium = (distance / totalHour).toFixed(2);
   const calory = ((velocityMedium * weight * caloryValue) * hourToMinutes).toFixed(0)
 
-  if(name !== '' && distance !== '' && weight !== '' ){
+  if (name !== '' && distance !== '' && weight !== '') {
     result.textContent = `Olá ${name}! Você percorreu ${distance} km em ${hour}h, ${arrayTime[1]} min e ${arrayTime[2]}s. Isso quer dizer que a sua velocidade média foi de ${velocityMedium} km/h, o que o corresponde a um pace de ${pace}min/km. Com ${weight}kg, você gastou cerca de ${calory} cal”`;
-  }else{
+  } else {
     alert('Para ter os resultados, preencha todos os campos.');
   }
 }
